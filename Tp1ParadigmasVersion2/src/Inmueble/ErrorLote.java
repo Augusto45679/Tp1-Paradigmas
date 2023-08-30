a0c27dd696188c4ed58d8c8eb3a25de05ab47520
Augusto45679 <109701343+Augusto45679@users.noreply.github.com>
2023-08-30 16:05:46 -0300

    2asa

--- Tp1ParadigmasVersion2/src/Inmueble/ErrorLote.java
+++ Tp1ParadigmasVersion2/src/Inmueble/ErrorLote.java
@@ -4,8 +4,9 @@ public class ErrorLote extends Exception( ){
 
 //msg no podes asignar mas de un edificio a un lote 
 
- public ErrorLote(){
-    super("No se puede asignar mas de un edificio a un lote");
+ public ErrorLote(String msg){
+ System.out.println(msg); 
 
+ 
  }
 }
